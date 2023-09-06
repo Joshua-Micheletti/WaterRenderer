ac3a181b2a23eb99dfb9e8a9b92d84397b27b259
Joshua-Micheletti <joshua.e.n.micheletti@gmail.com>
2023-09-06 03:51:07 +0200

    fix refactoring and add a proper gitignore

--- renderer/Renderer.py
+++ renderer/Renderer.py
@@ -10,36 +10,9 @@ class Renderer(metaclass=Singleton):
         glEnable(GL_BLEND)
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
 
-        self.vbo = 0
-        self.vbo = glGenBuffers(1)
-        glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
-
-
-        self.vao = glGenVertexArrays(1)
-        glBindVertexArray(self.vao)
-
-        print(self.vbo)
-
-        print(glGetIntegerv(GL_ARRAY_BUFFER_BINDING))
-
-        vertices = [-0.5, -0.5, 0.0, 1.0, 0.0, 0.0,
-                     0.5, -0.5, 0.0, 0.0, 1.0, 0.0,
-                    -0.5,  0.5, 0.0, 0.0, 0.0, 1.0,
-                     0.5,  0.5, 0.0, 1.0, 1.0, 1.0]
-
-        vertices = np.array(vertices, dtype=np.float32)
-
-        glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL_STATIC_DRAW)
-        
-        glEnableVertexAttribArray(0)
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(0))
-        glEnableVertexAttribArray(1)
-        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(12))
-
     def render(self):
         glClear(GL_COLOR_BUFFER_BIT)
 
         RendererManager().shaders["default"].use()
-        # glBindBuffer(GL_ARRAY_BUFFER, RendererManager().models["plane"].vbo)
         glDrawArrays(GL_TRIANGLE_STRIP, 0, 4)
         
--- renderer/model/Model.py
+++ renderer/model/Model.py
@@ -3,15 +3,17 @@ import numpy as np
 
 class Model:
     def __init__(self, vertices):
-        pass
-        # self.vbo = 0
-        # self.vbo = glGenBuffers(1)
-        # glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
+        self.vbo = 0
+        self.vbo = glGenBuffers(1)
+        glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
 
-        # vertices = np.array(vertices, dtype=np.float32)
-        # glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL_STATIC_DRAW)
+        self.vao = glGenVertexArrays(1)
+        glBindVertexArray(self.vao)
 
-        # glEnableVertexAttribArray(0)
-        # glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(0))
-        # glEnableVertexAttribArray(1)
-        # glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(12))
+        vertices = np.array(vertices, dtype=np.float32)
+        glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL_STATIC_DRAW)
+
+        glEnableVertexAttribArray(0)
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(0))
+        glEnableVertexAttribArray(1)
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(12))
